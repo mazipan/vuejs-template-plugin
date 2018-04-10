53517aa8bc0c0081cc0d86c244347df5cae68323
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-04-10 11:36:58 +0700

    fix extract css

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 var npm = require("./package.json")
 var CompressionPlugin = require("compression-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 var projectName = '{{ name }}'
 var libraryName = projectName.replace(/[^a-zA-Z0-9]/g, '')
 
@@ -19,95 +20,68 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'postcss-loader'
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader',
-          'postcss-loader'
-        ],
-      },
-      {
-        test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax',
-          'postcss-loader'
-        ],
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
+        exclude: /node_modules/,
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader',
-              'postcss-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax',
-              'postcss-loader'
-            ]
+            css: ExtractTextPlugin.extract({
+              use: 'css-loader',
+              fallback: 'vue-style-loader'
+            }),
+            scss: ExtractTextPlugin.extract({
+              use: 'css-loader!sass-loader',
+              fallback: 'vue-style-loader'
+            }),
+            sass: ExtractTextPlugin.extract({
+              use: 'css-loader!sass-loader?indentedSyntax',
+              fallback: 'vue-style-loader'
+            })
+            // scss: 'vue-style-loader!css-loader!sass-loader',
+            // sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      }
+  test: /\.js$/,
+    loader: 'babel-loader',
+      exclude: /node_modules/
+},
+{
+  test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+    loader: 'url-loader',
+      options: {
+    limit: 10000
+  }
+},
+{
+  test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+    loader: 'url-loader',
+      options: {
+    limit: 10000
+  }
+},
+{
+  test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url-loader',
+      options: {
+    limit: 10000
+  }
+}
     ]
   },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+resolve: {
+  alias: {
+    'vue$': 'vue/dist/vue.esm.js',
       'src': path.resolve(__dirname, 'dist'),
-      '@': path.resolve(__dirname, 'dist')
-    },
-    extensions: ['*', '.js', '.vue', '.json']
+        '@': path.resolve(__dirname, 'dist')
   },
-  externals: {
-    "vue": "Vue"
+  extensions: ['*', '.js', '.vue', '.json']
+},
+externals: {
+  'vue$': 'vue/dist/vue.esm.js',
   },
-  devtool: '#eval-source-map'
+devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
@@ -118,6 +92,9 @@ if (process.env.NODE_ENV === 'production') {
       'process.env': {
         NODE_ENV: '"production"'
       }
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css'
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
